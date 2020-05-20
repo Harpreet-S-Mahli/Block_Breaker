e5368c5c7aab8fbd2380437a1198a7ed65a62d0c
Harpreet S. Mahli <mahli.harpreet@yahoo.com>
2020-05-20 13:19:45 -0700

    Added audio onto the block, that'll create a new audio source for a moment to play the sound, then disappear

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 0.5f); //create a new audio file that'll play the sound on collision, then audio file will disappear. 
+                                                                      //where the audio file will play will be on the camera, as the distance to camera will determine how loud the sound will be, and how loud it'll be
         Destroy(gameObject);
     }
 }
