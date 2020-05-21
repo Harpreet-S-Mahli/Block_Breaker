7403e0dd55e45738d36ed30502c87ab82719a213
Harpreet S. Mahli <mahli.harpreet@yahoo.com>
2020-05-21 15:50:27 -0700

    Added a text UI to the Canvas prefab to display the players score when they destroy a block

--- Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // configuration parameters
     [Range(0.1f,10f)][SerializeField] float gameSpeed = 1f; //set a range on how high and low gamespeed can be, will become a slider in the inspector
     [SerializeField] int pointPerBlockDestroyed = 5;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,6 +27,7 @@ public class GameStatus : MonoBehaviour
 
     public void AddToScore()
     {
-        currentScore += pointPerBlockDestroyed; 
+        currentScore += pointPerBlockDestroyed;
+        scoreText.text =  currentScore.ToString();
     }
 }
